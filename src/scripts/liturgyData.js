be627f32e987d539acbe161567069b4bab726bd5
grejo.j <grejo.j@media.net>
2024-01-19 16:48:25 +0530

    liturgical committee table migrated

---
+++ src/scripts/liturgyData.js
@@ -0,0 +1,213 @@
+export const choirList = [
+    {
+        name: "Mr. Grejo Joby",
+        subText: "Choir Master/Keyboardist/Singer",
+    },
+    {
+        name: "Ms. Philcy Philip",
+        subText: "Singer",
+    },
+    {
+        name: "Ms. Anu Shaji",
+        subText: "Singer",
+    },
+    {
+        name: "Mr. Alen Abraham",
+        subText: "Singer",
+    },
+    {
+        name: "Ms. Jismi Mathew",
+        subText: "Singer",
+    },
+    {
+        name: "Ms. Angelmary Binoy",
+        subText: "Singer",
+    },
+    {
+        name: "Ms. Josna Vinod",
+        subText: "Singer",
+    },
+    {
+        name: "Mr. Joel Johnson",
+        subText: "Keyboardist",
+    },
+    {
+        name: "Mast. Mathew George",
+        subText: "Keyboardist",
+    },
+    {
+        name: "Mast. Annex Shaji",
+        subText: "Drummer",
+    },
+];
+
+export const altarAngelsList = [
+    {
+        name: "Mr. Binoy Chacko",
+        subText: "Animator",
+    },
+    {
+        name: "Annex Shaji",
+        subText: "President",
+    },
+    {
+        name: "Chris Binoy",
+        subText: "Secretary",
+    },
+    {
+        name: "Mathew George"
+    },
+    {
+        name: "Joshua Joseph"
+    },
+    {
+        name: "Neo Nisen"
+    },
+    {
+        name: "Philson Philip"
+    },
+    {
+        name: "Ronald Stephen"
+    },
+    {
+        name: "Zubin John"
+    },
+    {
+        name: "Aaron Sajimon"
+    },
+    {
+        name: "Reuben Sajimon"
+    },
+    {
+        name: "Evan Joseph"
+    },
+    {
+        name: "Lloyd Dennies"
+    },
+    {
+        name: "Jesher Sumoy"
+    },
+    {
+        name: "Noel Wilson"
+    },
+    {
+        name: "Nigel Wilson"
+    },
+    {
+        name: "Shaun Varghese"
+    },
+    {
+        name: "Helon Ajith"
+    },
+    {
+        name: "Rivan Mario"
+    },
+    {
+        name: "Alvin Ajith"
+    },
+    {
+        name: "Albin Ajith"
+    },
+];
+
+export const liturgyCommitteeList = [
+    {
+        name: "Mr. C. J. Antony",
+        subText: "Member"
+    },
+    {
+        name: "Mr. T. S. Ouseph",
+        subText: "Member"
+    },
+    {
+        name: "Mr. Jose Joseph",
+        subText: "Member"
+    },
+    {
+        name: "Mr. A. C. John",
+        subText: "Trustee"
+    },
+    {
+        name: "Mr. George Manachery",
+        subText: "Trustee"
+    },
+];
+
+export const prayerGroupList = [
+    {
+        name: "Mrs. Alphonsa Abraham",
+        subText: "Prayer Group Leader"
+    },
+    {
+        name: "Mr. Johnson Chittilappilly",
+        subText: "PRO"
+    },
+    {
+        name: "Mr. Binoy Chacko",
+        subText: "Retreat Co-ordinator"
+    },
+    {
+        name: "Mrs. Jessy Antony",
+        subText: "Member"
+    },
+    {
+        name: "Mrs. Siji Vincent",
+        subText: "Member"
+    },
+    {
+        name: "Mrs. Jincy Sajimon",
+        subText: "Member"
+    }
+];
+
+export const decorationCommitteeList = [
+    {
+        name: "Mrs. Jennifer Johnson",
+        subText: "Co-ordinator"
+    },
+    {
+        name: "Mrs. Alphonsa Abraham",
+    },
+    {
+        name: "Mrs. Ansorosy Biju",
+    },
+    {
+        name: "Mrs. Gilsy Jacob",
+    },
+    {
+        name: "Mrs. Jessy Maxy",
+    },
+    {
+        name: "Mrs. Jincy Sajimon",
+    },
+    {
+        name: "Mrs. Joyce Stephen",
+    },
+    {
+        name: "Mrs. Neoskia Jesbin",
+    },
+    {
+        name: "Mrs. Preena Francis",
+    },
+    {
+        name: "Mrs. Shaina Joseph",
+    },
+    {
+        name: "Mrs. Sherly Justine",
+    },
+    {
+        name: "Mrs. Shiby Philip",
+    },
+    {
+        name: "Mrs. Sophy Jison",
+    },
+    {
+        name: "Mrs. Suja Johny",
+    },
+    {
+        name: "Mrs. Tresa John",
+    },
+    {
+        name: "Mrs. Riya Sijo",
+    }
+];
