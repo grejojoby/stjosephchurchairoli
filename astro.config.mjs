897381e995c4f3ba5368c8139183f411651478de
grejojoby <grejo00@gmail.com>
2024-06-01 10:47:20 +0530

    sentry integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sentry({
+      dsn: "https://15438d63f56c44933d974d27574d2880@o4507351643389952.ingest.us.sentry.io/4507354965540864",
+      sourceMapsUploadOptions: {
+        project: "stjosephchurchairoli",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
+});
